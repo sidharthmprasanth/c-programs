4c2c57dfd86a0972103c872d158ce42238ec5c88
sidharthmprasanth <sidharthmprasanth@gmail.com>
2024-11-07 12:52:48 +0530

    Add files via upload

---
+++ 7-11-24/area-of-circle/main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float r;
+    printf("enter r :");
+    
+    
+    scanf("%f",&r);
+    printf("area of circle %f",3.14*r*r);
+
+    return 0;
+}
